be10a252f1f406c3b398621d681eb6cdd24d72f9
rosalinda <abreuparrarosalinda@gmail.com>
2024-10-25 17:00:06 -0400

    added vercel port

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
