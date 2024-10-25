d3fae40bf5eed811ebf6dff745c390ac66247a19
rosalinda <abreuparrarosalinda@gmail.com>
2024-10-25 19:46:04 -0400

    scrapping retry

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const axiosRetry = require('axios-retry');
-axiosRetry(axios, { retries: 3 });
 const cheerio = require('cheerio');
 const cors = require('cors');
 
@@ -17,67 +15,68 @@ let interestData = [];
 
 const url = 'https://clientebancario.bde.es/pcb/es/menu-horizontal/productosservici/relacionados/tiposinteres/guia-textual/tiposinteresrefe/Tabla_tipos_de_interes_legal.html';
 
-axios(url)
-    .then(response => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-
-      const meses = {
-        'enero': '01',
-        'febrero': '02',
-        'marzo': '03',
-        'abril': '04',
-        'mayo': '05',
-        'junio': '06',
-        'julio': '07',
-        'agosto': '08',
-        'septiembre': '09',
-        'octubre': '10',
-        'noviembre': '11',
-        'diciembre': '12'
-      };
-
-      $('table tbody tr').each(function(index, element) {
-        const row = {};
-        let year, start, end, interest;
-        $(element).find('td').each(function(tdIndex) {
-          let text = $(this).text().trim();
-          text = text.replace(/Abre en ventana nueva/, '');
-
-          const matchedYear = text.match(/\d{4}/);
-          if (matchedYear) {
-            year = matchedYear[0];
-            if (tdIndex === 0) {
-              if (text.includes('hasta')) {
-                const mes = text.match(/hasta (\w+)/)[1];
-                const mesNum = meses[mes.toLowerCase()];
-                start = `${year}-01-01`;
-                end = `${year}-${mesNum}-31`;
-              } else if (text.includes('desde')) {
-                const mes = text.match(/desde (\w+)/)[1];
-                const mesNum = meses[mes.toLowerCase()];
-                start = `${year}-${mesNum}-01`;
-                end = `${year}-12-31`;
-              } else {
-                start = `${year}-01-01`;
-                end = `${year}-12-31`;
-              }
-              row.start = start;
-              row.end = end;
+const fetchData = async () => {
+  try {
+    const response = await axios(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const meses = {
+      'enero': '01',
+      'febrero': '02',
+      'marzo': '03',
+      'abril': '04',
+      'mayo': '05',
+      'junio': '06',
+      'julio': '07',
+      'agosto': '08',
+      'septiembre': '09',
+      'octubre': '10',
+      'noviembre': '11',
+      'diciembre': '12'
+    };
+    $('table tbody tr').each(function(index, element) {
+      const row = {};
+      let year, start, end, interest;
+      $(element).find('td').each(function(tdIndex) {
+        let text = $(this).text().trim();
+        text = text.replace(/Abre en ventana nueva/, '');
+        const matchedYear = text.match(/\d{4}/);
+        if (matchedYear) {
+          year = matchedYear[0];
+          if (tdIndex === 0) {
+            if (text.includes('hasta')) {
+              const mes = text.match(/hasta (\w+)/)[1];
+              const mesNum = meses[mes.toLowerCase()];
+              start = `${year}-01-01`;
+              end = `${year}-${mesNum}-31`;
+            } else if (text.includes('desde')) {
+              const mes = text.match(/desde (\w+)/)[1];
+              const mesNum = meses[mes.toLowerCase()];
+              start = `${year}-${mesNum}-01`;
+              end = `${year}-12-31`;
+            } else {
+              start = `${year}-01-01`;
+              end = `${year}-12-31`;
             }
-          } else {
-            interest = parseFloat(text.replace(',', '.')).toFixed(4) / 100;
-            row.interest = interest;
+            row.start = start;
+            row.end = end;
           }
-        });
-        if (row.start && row.end && typeof row.interest !== 'undefined') {
-          interestData.push(row);
+        } else {
+          interest = parseFloat(text.replace(',', '.')).toFixed(4) / 100;
+          row.interest = interest;
         }
       });
-    })
-    .catch(error => {
-      console.log(error);
+      if (row.start && row.end && typeof row.interest !== 'undefined') {
+        interestData.push(row);
+      }
     });
+    console.log('Datos de interés:', interestData);
+  } catch (error) {
+    console.log('Error al conectar con el servidor:', error.message);
+  }
+};
+
+fetchData();
 
 app.post('/api/calculate', (req, res) => {
   const { amount, startDate, endDate } = req.body;
